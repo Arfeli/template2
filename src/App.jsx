73845a32c8c7e8d5afecba10989a6b2b1ba0d256
unknown <alejandro.arfeli@gmail.com>
2020-08-10 15:21:17 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react';
+import Navbar1 from './components./Navbar1';
+import Cover1 from './components./Cover1';
+import Counter1 from './components./Counter1';
+import Footer1 from './components./Footer1';
+
+function App() {
+  return (
+    <Fragment>
+      <Navbar1 />
+      <Cover1 />
+      <Counter1 />
+      <Footer1 />
+    </Fragment>
+  );
+}
+
+export default App;
